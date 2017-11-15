d9645c5e24caaa282f0dd7d8c380121c0904f5ff
Ubuntu <amersel@minictf.huid4tfkrtpufpc3nxszenipyb.xx.internal.cloudapp.net>
2017-11-15 07:31:18 +0000

    Initial Commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,4 +15,5 @@ if [ -n "$DATABASE_URL" ]
 fi
 
 echo "Starting CTFd"
-gunicorn --bind 0.0.0.0:8000 -w 1 'CTFd:create_app()' --access-logfile '/opt/CTFd/CTFd/logs/access.log' --error-logfile '/opt/CTFd/CTFd/logs/error.log'
+#gunicorn --bind 0.0.0.0:8000 -w 1 'CTFd:create_app()' --access-logfile '/opt/CTFd/CTFd/logs/access.log' --error-logfile '/opt/CTFd/CTFd/logs/error.log'
+gunicorn --bind 0.0.0.0:4000 -w 4 "CTFd:create_app()" --keyfile='/etc/letsencrypt/live/minictf.duckdns.org/privkey.pem' --certfile='/etc/letsencrypt/live/minictf.duckdns.org/cert.pem' --ca-certs='/etc/letsencrypt/live/minictf.duckdns.org/chain.pem' --access-logfile '/opt/CTFd/CTFd/logs/access.log' --error-logfile '/opt/CTFd/CTFd/logs/error.log'
